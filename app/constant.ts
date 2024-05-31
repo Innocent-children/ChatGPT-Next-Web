e4c7ce281fc3c037de3bb955f1a880c6d7cd9ecd
Innocent-children <Innocent_children@yeah.net>
2024-05-31 10:57:29 +0800

    注释掉Claude预置模型使其走ChatGPT协议

--- app/constant.ts
+++ app/constant.ts
@@ -173,8 +173,8 @@ const googleModels = [
 ];
 
 const anthropicModels = [
-  "claude-instant-1.2"
-  // "claude-2.0",
+  "claude-instant-1.2",
+  "claude-2.0"
   // "claude-2.1",
   // "claude-3-sonnet-20240229",
   // "claude-3-opus-20240229",
